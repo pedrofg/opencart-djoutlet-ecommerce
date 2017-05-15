8c624391b781d6df343615fb36a80698b21d8a94
Pedro Fernandes <pedro92.gf@gmail.com>
2017-05-14 21:17:09 -0400

    Main commit.

---
+++ opencarttest/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencarttest/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencarttest/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencarttest/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencarttest/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/opencarttest/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/opencarttest/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/opencarttest/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/opencarttest/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/opencarttest/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/opencarttest/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/opencarttest/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/opencarttest/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/opencarttest/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/opencarttest/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/opencarttest/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/opencarttest/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_djoutlet');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ opencarttest/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencarttest/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencarttest/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/opencarttest/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/opencarttest/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/opencarttest/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/opencarttest/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/opencarttest/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/opencarttest/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/opencarttest/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/opencarttest/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/opencarttest/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/opencarttest/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/opencarttest/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_djoutlet');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
